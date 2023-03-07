b8a086555e97281a3388a571e77f4cf8ebcfa6ad
Awudu-Jamal1 <78368357+Awudu-Jamal1@users.noreply.github.com>
2023-03-06 20:05:44 -0500

    added Next and previous function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,65 +1,96 @@
-import React from 'react';
-import Question from './component/Question';
-
+import React from "react";
+import Question from "./component/Question";
 
 function App() {
-  const [fetched,setFetched]=React.useState([])
-  const [solution,setSolution] =React.useState([])
-  const [show ,setShow] =React.useState(false)
+  const [fetched, setFetched] = React.useState([]);
+  const [solution, setSolution] = React.useState([]);
+  const [show, setShow] = React.useState(false);
+  const [next, setNext] = React.useState([0, 4]);
 
+  React.useEffect(() => {
+    const getQuiz = async () => {
+      let response = await fetch(
+        "https://opentdb.com/api.php?amount=12&type=multiple"
+      );
+      let quest = await response.json();
+      let result = await quest.results;
+      setFetched(result);
+    };
+    getQuiz();
+  }, []);
 
-  React.useEffect(()=>{
-    const getQuiz=async() =>{
-      let response =  await fetch('https://opentdb.com/api.php?amount=10&type=multiple')
-      let quest=await response.json()
-      let result = await quest.results
-      setFetched(result)
-    }
-    getQuiz()
+  const solutions = (i, a, reply) => {
+    let m = solution.find((e) => e.value === i);
+    const ans = {
+      value: i,
+      answer: a,
+      reply: reply,
+    };
+    setSolution((prev) =>
+      m
+        ? prev.map((e) => {
+            return e.value === i ? { ...e, reply: reply } : e;
+          })
+        : [...prev, ans]
+    );
+  };
+  const handleNext = () => {
+    setNext([next[1], next[1] + 4]);
+  };
 
+  const handlePrev = () => {
+    setNext([next[0] - 4, next[0]]);
+  };
 
+  const evaluteHandler = () => {
+    setShow(true);
+  };
+  const restartHandler = () => {
+    window.location.reload(false);
+  };
 
-  },[])
+  const disabled = solution.length === 12 ? true : false;
 
-  const solutions=(i,a,reply)=>{
-    let m =solution.find(e=>e.value ===i)
-const ans ={
-  value:i,
-  answer:a,
-  reply:reply
-}
-setSolution(prev =>m?
-   prev.map(e =>{
-   return e.value ===i?
-    {...e,reply:reply}:e
-
-  }
-  ):[ ...prev,ans ]
-
-)
-
-  }
-
-  console.log(solution.length)
-  const evaluteHandler=()=>{
-   setShow(true)
-  }
-  const restartHandler=()=>{
-    window.location.reload(false)
-   }
-
-  const  disabled = solution.length !==10 ? true:false
-
-  const allQuestions = fetched.map((quest,i )=>i<5?<Question key={i} value={i} show={show}
-  quest={quest} solutions={solutions} />:'')
+  const allQuestions = fetched.map((quest, i) =>
+    i >= next[0] && i < next[1] ? (
+      // <li key={i} >
+      <Question
+        key={i}
+        value={i}
+        show={show}
+        quest={quest}
+        solutions={solutions}
+      /> /*</li>*/
+    ) : (
+      ""
+    )
+  );
   return (
     <div className="App">
-     <div> {allQuestions}</div>
-     <div>{show?<button  onClick={restartHandler} >Restart</button>:<button disabled={disabled}
-     onClick={evaluteHandler} > Evalute</button>}</div>
+      <div className="mainApp">
+        <ol> {allQuestions}</ol>
+      </div>
+
+      <div className="movebox">
+        <div>
+          {next[0] !== 0 && <button onClick={handlePrev}>Previous</button>}
+        </div>
+        <div>
+          {show ? (
+            <button onClick={restartHandler}>Restart</button>
+          ) : disabled ? (
+            <button onClick={evaluteHandler}> Evalute</button>
+          ) : (
+            ""
+          )}
+        </div>
+
+        <div>
+          {next[1] !== 12 && <button onClick={handleNext}> Next</button>}
+        </div>
+      </div>
     </div>
   );
 }
-
 
 export default App;
--- my-app/src/component/Question.js
+++ my-app/src/component/Question.js
@@ -1,42 +1,59 @@
-import React, { useState } from 'react';
-const Question =(props)=>{
-console.log()
-const [response,setResponse]=useState('')
-    // const [change,setChange] =useState(false)
+import React, { useState } from "react";
+const Question = (props) => {
+  console.log();
+  const [response, setResponse] = useState("");
 
-    const {value,question,correct_answer,incorrect_answers,show}=props.quest
-// console.log(incorrect_answers.length)
+  const { question, correct_answer, incorrect_answers } = props.quest;
 
-    // console.log([correct_answer,incorrect_answers])
-    if(incorrect_answers.length <4){
-    const randomize=(a,b)=>{
-            let rans =Math.ceil(Math.random()*4)
+  if (incorrect_answers.length < 4) {
+    const randomize = (a, b) => {
+      let rans = Math.ceil(Math.random() * 4);
 
-        a.splice(rans,0,b)
-    }
-    randomize(incorrect_answers,correct_answer)}
+      a.splice(rans, 0, b);
+    };
+    randomize(incorrect_answers, correct_answer);
+  }
 
-const handleResponse=(i,a,reply,btn)=>{
+  const handleResponse = (i, a, reply, btn) => {
+    setResponse([reply, btn]);
+    props.solutions(i, a, reply);
+  };
 
-    setResponse([reply,btn])
-    props.solutions(i,a,reply)
-}
+  let correct = props.show ? correct_answer : "";
+  console.log(response[1]);
+  // console.log(show,correct)
+  const decsion = response === correct_answer ? "Correct" : "Wrong";
+  const objective = incorrect_answers.map((obj, i) => (
+    <button
+      disabled={props.show}
+      className={`select-button ${
+        correct === obj
+          ? "right"
+          : i === response[1]
+          ? "selected-note"
+          : "button-color"
+      }`}
+      onClick={() => handleResponse(props.value, correct_answer, obj, i)}
+      key={i}
+    >
+      {obj}
+    </button>
+  ));
+  return (
+    <div className="main">
+      <h4>
+        {props.value + 1}. {question}
+      </h4>
 
-let correct = props.show ? correct_answer:''
-console.log(response[1])
-// console.log(show,correct)
-const decsion = response === correct_answer? 'Correct':'Wrong'
-const objective=incorrect_answers.map((obj,i) =><button disabled={props.show}
-className={`select-button ${correct ===obj?"right":i=== response[1] ? "selected-note" : "button-color" }`}
-onClick={()=>handleResponse(props.value,correct_answer,obj,i)} key={i}
->{obj}</button>)
-    return(
-       <div className='main'>
-        <h4>{question}</h4>
+      <div className="box">
+        {" "}
+        <div className="objs">
+          {objective}
+          {props.show ? correct_answer : ""}
+        </div>{" "}
+      </div>
+    </div>
+  );
+};
 
-        <div className='box'> <div className='objs' >{objective}{props.show?correct_answer:''}</div> </div>
-       </div>)
-
-}
-
-export default Question
+export default Question;
